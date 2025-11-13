3de21a3197631ab462482e43f3c22e63cf240ad7
Bell <nbelbobb@c889f3b52fa5.ant.amazon.com>
2025-11-12 17:08:35 -0800

    Fix Render deployment - remove Amazon internal packages and fix paths

--- server/index.js
+++ server/index.js
@@ -439,10 +439,11 @@ app.get('/api/featured-recipes', async (req, res) => {
 
 // Serve static files from React build
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../dist')));
+  const distPath = path.join(__dirname, '..', 'dist');
+  app.use(express.static(distPath));
   
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
+    res.sendFile(path.join(distPath, 'index.html'));
   });
 }
 
